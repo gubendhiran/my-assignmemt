ea4853d8140572dbb5f78c9eab34041900436dbf
gubendhiran <115517143+gubendhiran@users.noreply.github.com>
2022-10-18 12:31:19 +0530

    week2 day1

---
+++ ToUpperCase.java
@@ -0,0 +1,11 @@
+  package week.day1;
+
+public class ToUpperCase {
+ 
+	public static void main(String args[]){  
+		String s1=" string";  
+		String s1upper=s1.toUpperCase();  
+		System.out.println(s1upper);  
+		}}  
+
+
